50923a4ff270d6e25e2a0aca29a5daccff72bf83
Joseph Karnafel <josephkarnafel@gmail.com>
2020-10-16 15:29:13 -0700

    add passing test for callback which titleizes product names

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,4 +6,9 @@ describe Product do
   it { should validate_presence_of :name}
   it { should validate_presence_of :cost}
   it { should validate_presence_of :country_of_origin}
+
+  it("titleizes the name of the product") do
+    product = Product.create!({name: "iron shovel", cost: 5.00, country_of_origin: "America"})
+    expect(product.name()).to(eq("Iron Shovel"))
+  end
 end
