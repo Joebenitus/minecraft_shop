16906db39e7a652b25da8bd251f704b24413df7b
Joseph Karnafel <josephkarnafel@gmail.com>
2020-10-16 14:14:20 -0700

    add passing test for validation of presence of cost and country_of_origin properties of product class

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates :name, presence: true
+  validates :cost, presence: true
+  validates :country_of_origin, presence: true
 end
