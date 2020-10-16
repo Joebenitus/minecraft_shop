b99bf2ae2a330b717a40940efae61ebb553e8e07
Joseph Karnafel <josephkarnafel@gmail.com>
2020-10-16 13:53:07 -0700

    add sample data for reviews class

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Product.destroy_all
+Review.destroy_all
 
 50.times do |index|
-  Product.create!(name: Faker::Games::Minecraft.item,
+  product = Product.create!(name: Faker::Games::Minecraft.item,
                 cost: rand(1.0..100.0).round(2),
                 country_of_origin: Faker::Address.country)
+  5.times do |review|
+    Review.create!(author: Faker::FunnyName.name,
+                  content_body: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 4).chop,
+                  rating: rand(1..5),
+                  product_id: product.id.to_i)
+  end
 end
 
-p "Created #{Product.count} spices"
+p "Created #{Product.count} products"
